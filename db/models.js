38b6e40bd16d2f087b8d82c8edea167c4af6b2ca
jesuscolmenaresp <colmenaresjesus94@gmail.com>
2023-11-11 02:08:30 -0300

    actualizacion

--- db/models.js
+++ db/models.js
@@ -1,12 +1,22 @@
 const { name } = require('ejs');
 const db = require('./connection');
+
 let querys = {
 getCategories: 'SELECT * FROM categories',
+getCategoriesID: 'SELECT * FROM categories WHERE id = ?',
 getProducts: 'SELECT * FROM products',
-getProductsID: 'SELECT * FROM products WHERE id =?',
+getProductsID: 'SELECT * FROM products WHERE id = ?',
 insertProducts: 'INSERT INTO products (name, code, price, description, color, guy, category_id) VALUES(?, ?, ?, ?, ?, ?, ?)',
-updateProducts: 'UPDATE Products SET name = ?, code = ?, price= ?, description = ?, color = ?, guy = ?, category_id = ? WHERE id = ?',
-deleteProducts: 'DELETE FROM products WHERE id = ?'
+updateProducts: 'UPDATE products SET name = ?, code = ?, price= ?, description = ?, color = ?, guy = ?, category_id = ? WHERE id = ?',
+deleteProducts: 'DELETE FROM products WHERE id = ?',
+insertCategories: 'INSERT INTO categories (name) VALUES(?)',
+updateCategories: 'UPDATE categories SET name = ? WHERE id = ?',
+deleteCategories: 'DELETE FROM categories WHERE id = ?',
+insertimagen: 'INSERT INTO image (url, product_id, destacado) VALUES(?, ?, ?)',
+getimagenID: 'SELECT * FROM image WHERE id = ?',
+getimagen: 'SELECT * FROM image',
+updateimagen: 'UPDATE image SET url = ?, producto_id = ?, destacado = ? WHERE id = ?',
+deleteimagen: 'DELETE FROM image WHERE id = ?'
 }
 
 module.exports = {
@@ -20,21 +30,38 @@ module.exports = {
 },
 getProductsID(id) {
     return new Promise((resolve, reject)=>{
-        db.run(querys.getProductsID, [id], (err, rows) => { 
+        db.all(querys.getProductsID, [id], (err, rows) => { 
             if(err) reject(err);
             console.log(rows)
             resolve(rows);
           });
       });
 },
- 
 getCategories(){
     return new Promise((resolve, reject) => {
        db.all(querys.getCategories, (err, rows) => { 
           if(err) reject(err);
           resolve(rows);
+        });
+    });
+},
+getCategoriesID(id) { 
+    return new Promise((resolve, reject)=>{ 
+      db.all(querys.getCategoriesID, [id], (err, rows) => {  
+        if(err) reject(err); 
+        console.log(rows)
+        resolve(rows); 
+      }); 
+    }); 
+  },
+insertCategories(name){
+    return new Promise((resolve, reject) => {
+        db.run(querys.insertCategories, [name], (err) => {
+            if(err) reject(err);
+            console.log("LA CATEGORIA INGRESADA FUE: ", name);
+            resolve();
         })
-    })
+    });
 },
 
 insertProducts(name, code, price, description, color, guy, category_id){
@@ -47,8 +74,16 @@ insertProducts(name, code, price, description, color, guy, category_id){
     });
 },
 updateProducts(id, name, code, price, description, color, guy, category_id){
-    return new Promise((resolve, reject) => {
-        db.run(querys.updateProducts, [name, code, price, description, color, guy, category_id, id], (err)=> {
+    return new Promise((resolve, reject)=>{
+        db.run(querys.updateProducts, [name, code, price, description, color, guy, category_id, id], (err)=>{
+            if(err) reject(err);
+            resolve();
+        })
+    });
+},
+updateCategories(id, name){
+    return new Promise((resolve, reject)=>{
+        db.run(querys.updateCategories, [name, id], (err)=>{
             if(err) reject(err);
             resolve();
         })
@@ -62,5 +97,54 @@ deleteProducts(id){
             resolve(rows);
           });
       });
+},
+deleteCategories(id){
+    return new Promise((resolve, reject)=>{
+        db.run(querys.deleteCategories, [id], (err, rows) => { 
+            if(err) reject(err);
+            console.log(rows)
+            resolve(rows);
+          });
+      });
+},
+getimagen(){
+    return new Promise((resolve, reject) => {
+        db.all(querys.getimagen, (err, rows) => {
+            if(err) reject(err);
+            resolve(rows);
+        })
+    })
+},
+getimagenID(id){
+    return new Promise((resolve, reject) => {
+        db.all(querys.getimagenID, [id], (err, rows) => {
+            if(err) reject(err);
+            resolve(rows);
+        })
+    })
+},
+insertimagen(url, producto_id, destacado){
+    return new Promise((resolve, reject) => {
+        db.run(querys.insertimagen, [url, producto_id, destacado], (err) => {
+            if(err) reject(err);
+            resolve();
+        })
+    })
+},
+updateimagen(id, url, producto_id, destacado){
+    return new Promise((resolve, reject) => {
+        db.run(querys.updateimagen, [url, producto_id, destacado, id], (err) => {
+            if(err) reject(err);
+            resolve();
+        })
+    })
+},
+deleteimagen(id){
+    return new Promise((resolve, reject) => {
+        db.run(querys.deleteimagen, [id], (err) => {
+            if(err) reject(err);
+            resolve();
+        })
+    })
 }
 }
--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,19 @@ const db = require('../db/models');
 require('dotenv').config();
 let login = false;
 
-router.get('/', (req, res) =>{
- if(!login){
-  res.render('login');
- }else{
-  db.getProducts()
-  .then(data => {
-    res.render('index', {products: data});
-})
-.catch(err => {
-  res.render('index', {products: []});
-});
- }
+router.get('/', (req, res) => {
+  if(!login){
+    res.render('login');
+  } else {
+    Promise.all([db.getProducts(), db.getCategories()]) 
+      .then(([products, categories]) => {
+        res.render('index', { products: products, categories: categories });
+      })
+      .catch(err => {
+        console.error(err);
+        res.render('index', { products: [], categories: [] }); 
+      });
+  }
 });
 
 router.get('/', (req, res) => {
@@ -26,38 +27,55 @@ router.get('/', (req, res) => {
         res.render('index', {products: data});
     })
     .catch(err => {
-      res.render('index', { products: []});
+      res.render('index', {products: []});
     });
   });
 
-router.get('/categories', (req, res) => {
+router.get('/', (req, res) => {
     db.getCategories()
     .then(data => {
-        console.log(data);
-        res.render('index', {categories: data });
+        res.render('index', {categories: data});
+      })
+      .catch(err => {
+        res.render('index', {categories: []});
+      });
     });
-});
 
 router.get('/insert', (req, res) => {
-     res.render('insert');
+  res.render('insert');
+});
+
+router.get('/insertcategories', (req, res) => {
+  res.render('insertcategories');
 });
 
 router.post('/insert-product', (req, res) => {
-  const {name, code, price, description, color, guy, category_id} = req.body;
-  db.insertProducts(name, code, price, description, color, guy, category_id)
+const {name, code, price, description, color, guy, category_id} = req.body;
+db.insertProducts(name, code, price, description, color, guy, category_id)
+.then(() => {
+res.redirect('/');
+})
+.catch(err => {
+ console.log(err);
+});
+})
+
+router.post('/insert-categorie', (req, res) => {
+  const {name} = req.body;
+  db.insertCategories(name)
   .then(() => {
   res.redirect('/');
   })
   .catch(err => {
     console.log(err);
 });
-});
+})
 
-router.get('/edit/:id', (req, res) => { 
+router.get('/edit/:id', (req, res)=>{ 
   const id = req.params.id;
-  console.log(id)
+  console.log(id);
   db.getProductsID(id)
-  .then(data => {
+.then(data => {
     res.render('edit', {product: data[0]});
     })
     .catch(err => {
@@ -66,9 +84,33 @@ router.get('/edit/:id', (req, res) => {
   });
 });
 
-router.get('/edit/:id', (req, res) => { 
-  const {id,name, code, price, description, color, guy, category_id} = req.body;
+router.post('/edit/', (req, res)=>{ 
+  const {id, name, code, price, description, color, guy, category_id} = req.body;
   db.updateProducts(id, name, code, price, description, color, guy, category_id)
+  .then(() => {
+  res.redirect('/');
+  })
+  .catch(err => {
+    console.log(err);
+});
+});
+
+router.get('/editcategories/:id', (req, res)=>{
+  const id = req.params.id;
+  console.log(id);
+  db.getCategoriesID(id)
+.then(data => {
+   res.render('editcategories', {categorie: data[0]});
+    })
+    .catch(err => {
+      console.log(err);
+      res.render('editcategories', {categorie: []});
+    });
+});
+
+router.post('/editcategories/', (req, res)=>{ 
+  const {id, name} = req.body;
+  db.updateCategories(id, name)
   .then(() => {
   res.redirect('/');
   })
@@ -88,6 +130,17 @@ router.get('/delete/:id', (req, res) =>{
 });
 });
 
+router.get('/deletes/:id', (req, res) =>{
+  const id = req.params.id;
+  db.deleteCategories(id)
+  .then(() => {
+    res.redirect('/');
+  })
+  .catch(err => {
+    console.log(err);
+});
+});
+
 router.post('/login', (req, res) =>{
   const {user, pass} = req.body;
   if(user == process.env.USER_ADMIN && pass == process.env.PASS_ADMIN){
@@ -98,5 +151,67 @@ router.post('/login', (req, res) =>{
     res.redirect('/');
   }
   })
+
+router.get('/tabimagen', (req, res) => {
+  db.getimagen()
+  .then(data => {        
+  console.log(data)
+    res.render('tabimagen', {image: data });
+    })
+    .catch(err => {
+        res.render('tabimagen', {image: [] });
+    })
+  });
+  
+router.get('/img', (req, res) => {
+  res.render('img');
+})
+
+router.post('/img', (req, res) => {
+  const {url, producto_id, destacado} = req.body;
+  console.log(url, producto_id, destacado);
+  db.insertProducts(url, producto_id, destacado)
+  .then(() => {
+    res.redirect('tabimagen')
+    })
+    .catch(err => {
+    console.log(err);
+    })
+  });
+  
+router.post('/editima/', (req, res)=>{
+  const {id, url, producto_id, destacado} = req.body;
+  db.updateimagen(id, url, producto_id, destacado)
+  .then(() =>{
+  res.redirect('/tabimagen');
+  })
+  .catch(err =>{
+    console.log(err);
+    })
+  });
+
+router.get('/editima/:id', (req, res)=>{
+  const id = req.params.id
+  db.getimagenID(id)
+  .then(data =>{
+  console.log(data)
+    res.render('editima', {image: data[0]})
+    })
+    .catch(err =>{
+    console.log(err);
+    res.render('editima', {image: []})
+    })
+  }); 
+
+router.get('/deleteima/:id', (req, res)=>{
+  const id = req.params.id;
+  db.deleteimagen(id)
+  .then(() => {
+  res.redirect('/tabimagen');
+    })
+    .catch(err => {
+    console.log(err);
+    });
+  })      
 
 module.exports = router;
